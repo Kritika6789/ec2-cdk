4b6feb4ad63e4085d3c919c8a587e997184ebccb
Kritika6789 <kritikajain1805@gmail.com>
2025-06-16 16:44:54 +0530

    changes

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import aws_cdk as cdk
+import os
 from ec2_cdk.ec2_cdk_stack import Ec2CdkStack
 
 app = cdk.App()
-Ec2CdkStack(app, "Ec2CdkStack" ) # temporary token from GitHub)
+Ec2CdkStack(app, "Ec2CdkStack",env=cdk.Environment(
+        account=os.environ["459685751152"],
+        region=os.environ["ap-south-1"]
+    ) ) # temporary token from GitHub)
 app.synth()
 
