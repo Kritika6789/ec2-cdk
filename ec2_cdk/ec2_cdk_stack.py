11ee8f2f8e709e935e7bdaeb2404135d45010478
Kritika6789 <kritikajain1805@gmail.com>
2025-06-17 14:22:30 +0530

    changes

--- ec2_cdk/ec2_cdk_stack.py
+++ ec2_cdk/ec2_cdk_stack.py
@@ -32,20 +32,7 @@ class Ec2CdkStack(Stack):
         )
 
         # UserData for installing GitHub Runner
-        user_data = ec2.UserData.for_linux()
-        user_data.add_commands(
-            "cd /opt",
-            "mkdir actions-runner && cd actions-runner",
-            "curl -o actions-runner-linux-x64-2.325.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.325.0/actions-runner-linux-x64-2.325.0.tar.gz",
-            "tar xzf ./actions-runner-linux-x64-2.325.0.tar.gz",
-            "sudo chown -R ec2-user:ec2-user /opt/actions-runner",
-            "cd /opt/actions-runner",
-            "chmod +x config.sh",
-            # Replace below with your actual runner registration token
-            "./config.sh --url https://github.com/Kritika6789/ec2-cdk --token BAP5FXCQNDA3EFJ3HZBE45DIKALWW --name ec2-instance-12 --labels ec2-instance-12 --unattended",
-            "./run.sh"
-        )
-
+    
         # EC2 Instance
         instance = ec2.Instance(
             self, "GithubRunnerInstance",
@@ -53,8 +40,7 @@ class Ec2CdkStack(Stack):
             machine_image=ec2.MachineImage.latest_amazon_linux2(),
             vpc=vpc,
             security_group=sg,
-            role=role,
-            user_data=user_data
+            role=role
         )
 
 
