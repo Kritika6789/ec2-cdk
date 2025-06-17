49199af1f67caec4b6cf30787407c2375d9562f1
Kritika6789 <kritikajain1805@gmail.com>
2025-06-17 15:35:25 +0530

    created a Proper folder structure

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import aws_cdk as cdk
 import os
-from resources.ec2_cdk.ec2_cdk_stack import Ec2CdkStack
-from resources.s3_bucket.s3_cdk_stack import S3CdkStack
+from resources.ec2_cdk_stack import Ec2CdkStack
+from resources.s3_cdk_stack import S3CdkStack
 app = cdk.App()
 account = os.environ.get("CDK_DEFAULT_ACCOUNT")
 region = os.environ.get("CDK_DEFAULT_REGION")
